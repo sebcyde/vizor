807ba91273c3b725833c950155769c55fd8c8f33
Seb <sebcyde@hotmail.com>
2022-07-23 00:50:45 +0100

    ''

--- src/Components/Homepage/Homepage.jsx
+++ src/Components/Homepage/Homepage.jsx
@@ -12,6 +12,7 @@ function Homepage() {
 	const [res, setRes] = useState([]);
 
 	// Working - don't abuse
+	// finish card styling FIRST
 
 	// const fetchRequest = async () => {
 	// 	const data = await fetch(
@@ -28,31 +29,6 @@ function Homepage() {
 	// 		console.log(res);
 	// 	});
 	// }, []);
-
-	<Col m={6} s={12}>
-		<Card
-			closeIcon={<Icon>close</Icon>}
-			header={
-				<CardTitle
-					image="https://materializecss.com/images/sample-1.jpg"
-					reveal
-					waves="light"
-				/>
-			}
-			reveal={
-				<p>
-					Here is some more information about this product that is only revealed
-					once clicked on.
-				</p>
-			}
-			revealIcon={<Icon>more_vert</Icon>}
-			title="Card Title"
-		>
-			<p>
-				<a href="#">This is a link</a>
-			</p>
-		</Card>
-	</Col>;
 
 	return (
 		<div className="HomepageContainer">
