7e93babbc796d8a9ad93801bb0b3726ac5d1e6a9
Seb <sebcyde@hotmail.com>
2022-07-22 20:51:07 +0100

    Initial Commit

---
+++ src/Components/LoadingScreen/LoadingScreen.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './LoadingScreen.css';
+
+function LoadingScreen() {
+	return (
+		<div>
+			<h2>Loading Screen</h2>
+		</div>
+	);
+}
+
+export default LoadingScreen;
