804d311de52bb4c000e56f701b6666a99254b04d
Seb <sebcyde@hotmail.com>
2022-08-06 22:58:20 +0100

    progress on Messages page

--- src/Components/Messages/Messages.jsx
+++ src/Components/Messages/Messages.jsx
@@ -1,11 +1,50 @@
 import React from 'react';
 import Nav from '../Navbar/TopNav/Nav';
+import { Tab, Tabs } from 'react-materialize';
+import { Link, useNavigate } from 'react-router-dom';
 
 function Messages() {
+	const navigate = useNavigate();
+
 	return (
 		<div className="MessagesContainer">
 			<Nav />
-			<h2>Messages</h2>
+			<Tabs className="tab-demo z-depth-1" scope="tabs-22">
+				<Tab
+					active
+					options={{
+						duration: 300,
+						onShow: null,
+						responsiveThreshold: Infinity,
+						swipeable: false,
+					}}
+					title="Priority"
+				>
+					<h2>Priority</h2>
+				</Tab>
+				<Tab
+					options={{
+						duration: 300,
+						onShow: null,
+						responsiveThreshold: Infinity,
+						swipeable: false,
+					}}
+					title="Other"
+				>
+					<h2>Other</h2>
+				</Tab>
+				<Tab
+					options={{
+						duration: 300,
+						onShow: null,
+						responsiveThreshold: Infinity,
+						swipeable: false,
+					}}
+					title="Spam"
+				>
+					<h2>Spam</h2>
+				</Tab>
+			</Tabs>
 		</div>
 	);
 }
